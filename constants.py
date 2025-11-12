5a6aab1638e30d9142cb14b2702679cfb7346df0
Cole <oftedahl.cole@gmail.com>
2025-11-12 10:01:01 -0600

    added main method

--- constants.py
+++ constants.py
@@ -1,2 +1,8 @@
+from enum import Enum
+
 TRAINING_DATA_PATH = "Data/"
 VAL_DATA_PATH = "Data/"
+class MODEL_OPTIONS(Enum):
+  BERT_MULTILINGUAL = "https://huggingface.co/nlptown/bert-base-multilingual-uncased-sentiment"
+  SENTIMENT_ANALYSIS_BERT = "https://huggingface.co/MarieAngeA13/Sentiment-Analysis-BERT"
+  TWITTER_SENTIMENT_ANALYSIS_BERT = "https://huggingface.co/cardiffnlp/twitter-roberta-base-sentiment-latest"
--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+from FileParser import FileParser
+from constants import TRAINING_DATA_PATH, VAL_DATA_PATH, MODEL_OPTIONS
+from LanguageModel import LanguageModel
+
 if __name__ == '__main__':
-  pass
+  fileParser = FileParser()
+  trainingData = fileParser.parseFile(TRAINING_DATA_PATH)
+  lm = LanguageModel(MODEL_OPTIONS.BERT_MULTILINGUAL)
+  lm.train(trainingData)
