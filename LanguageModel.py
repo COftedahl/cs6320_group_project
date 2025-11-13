b7d368b9a75436533c6220f9560a281d2c3bcdaf
Cole <oftedahl.cole@gmail.com>
2025-11-13 13:50:55 -0600

    implemented lm

--- LanguageModel.py
+++ LanguageModel.py
@@ -1,12 +1,34 @@
-from transformers import pipeline
+from transformers import pipeline, infer_device, AutoModelForSequenceClassification, AutoTokenizer, Trainer, TrainingArguments, DataCollatorWithPadding
+
+# Reference: https://huggingface.co/docs/transformers/quicktour
 
 class LanguageModel(): 
 
   def __init__(self, modelSource): 
-    self.pipe = pipeline("text-classification", model=modelSource)
+    self.modelSource = modelSource
+    self.device = infer_device()
+    self.pipe = pipeline("text-classification", model=modelSource, device=self.device)
+    self.model = AutoModelForSequenceClassification.from_pretrained(modelSource)
+    self.tokenizer = AutoTokenizer.from_pretrained(modelSource)
+    self.collocator = DataCollatorWithPadding(tokenizer=self.tokenizer)
 
-  def train(self, data): 
-    pass
+  def train(self, trainData, testData, trainingArgs = TrainingArguments(
+      output_dir="Data",
+      learning_rate=2e-5,
+      per_device_train_batch_size=8,
+      per_device_eval_batch_size=8,
+      num_train_epochs=2,
+      push_to_hub=False,
+  )): 
+    trainer = Trainer(
+      model=self.model,
+      args=trainingArgs,
+      train_dataset=trainData,
+      eval_dataset=testData,
+      tokenizer=self.tokenizer,
+      data_collator=self.collocator,
+    )
+    trainer.train()
 
   def test(self, data): 
-    pass
+    return self.pipeline(data)
