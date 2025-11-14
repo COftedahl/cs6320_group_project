1b3b3be30507669c505a61e94b414a0e1f9899e7
beesterman <72782957+beesterman@users.noreply.github.com>
2025-11-13 23:26:31 -0600

    Added in cleaned data. Raw data is not included in this commit as its too large.

--- constants.py
+++ constants.py
@@ -1,7 +1,9 @@
 from enum import Enum
 
-TRAINING_DATA_PATH = "Data/"
-VAL_DATA_PATH = "Data/"
+RAW_DATA_PATH = "./data/raw/"
+CLEANED_DATA_PATH = "./data/clean/"
+FINAL_DATA_PATH = "./data/final/"
+USER_AT_TOKEN = "<atToken>"
 class MODEL_OPTIONS(Enum):
   BERT_MULTILINGUAL = "https://huggingface.co/nlptown/bert-base-multilingual-uncased-sentiment"
   SENTIMENT_ANALYSIS_BERT = "https://huggingface.co/MarieAngeA13/Sentiment-Analysis-BERT"
